515502b8aa901a84083aa1f1044c1c7859b02c66
Heri Andria Herdiana <andri@niagahoster.co.id>
2018-04-16 15:55:50 +0700

    modified updateUser

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -32,7 +32,7 @@ class UserController extends Controller
         $user = User::find($id);
         $user->first_name = $request->input('first_name');
         $user->last_name = $request->input('last_name');
-//        $user->address = $request->input('address');
+        $user->address = $request->input('address');
 //        $user->email = $request->input('email');
 //        $user->birth_date = $request->input('birth_date');
 //        $user->weight = $request->input('weight');
