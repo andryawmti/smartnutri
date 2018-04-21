75914b010f8b794018982826eb0d510fa3b465e0
Heri Andria Herdiana <andri@niagahoster.co.id>
2018-04-21 16:37:20 +0700

    cek

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -156,30 +156,30 @@ class UserController extends Controller
     {
         $email = $request->input('email');
         return $email;
-        $user = User::where('email', '=', $email)->first();
-        if ( count($user) > 0 ) {
-            $newPassword = str_random(8);
-            $send = Mail::to("andri@niagahoster.co.id")->send(new ResetPassword($newPassword));
-            if (Mail::failures()) {
-                return json_encode(array(
-                    "error" => true,
-                    "message" => "Email was not sent"
-                ));
-            }
-
-            $user->password = Hash::make($newPassword);
-            $user->save();
-
-            return json_encode(array(
-                "error" => false,
-                "message" => "Message has been sent"
-            ));
-        }
-
-        return json_encode(array(
-            "error" => true,
-            "message" => "Email not found"
-        ));
+//        $user = User::where('email', '=', $email)->first();
+//        if ( count($user) > 0 ) {
+//            $newPassword = str_random(8);
+//            $send = Mail::to("andri@niagahoster.co.id")->send(new ResetPassword($newPassword));
+//            if (Mail::failures()) {
+//                return json_encode(array(
+//                    "error" => true,
+//                    "message" => "Email was not sent"
+//                ));
+//            }
+//
+//            $user->password = Hash::make($newPassword);
+//            $user->save();
+//
+//            return json_encode(array(
+//                "error" => false,
+//                "message" => "Message has been sent"
+//            ));
+//        }
+//
+//        return json_encode(array(
+//            "error" => true,
+//            "message" => "Email not found"
+//        ));
 
     }
 
