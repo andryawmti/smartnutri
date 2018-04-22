da80f29ae4ed6db9c6a54a7f70de60af749fb1b6
Heri Andria Herdiana <andri@niagahoster.co.id>
2018-04-22 20:52:08 +0700

    fix controller error

--- app/Http/Controllers/UserControllerApi.php
+++ app/Http/Controllers/UserControllerApi.php
@@ -19,7 +19,7 @@ class UserControllerApi extends Controller
      */
     public function __construct()
     {
-        $this->middleware(['auth:user-api'],['except'=>['index','resetPassword','signUp']]);
+        $this->middleware("auth:user-api")->except(['index','resetPassword','signUp']);
     }
 
     /**
