0948d11f9dc5245a127d2fe45fb25ec2f55cc404
Heri Andria Herdiana <andri@niagahoster.co.id>
2018-04-22 21:47:05 +0700

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -72,6 +72,7 @@ class UserController extends Controller
 
     public function signUp(Request $request)
     {
+        return "fuck you";
         $user = new User();
         $user->first_name = $request->input("first_name");
         $user->last_name = $request->input("last_name");
